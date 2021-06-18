a7c4b4b02803cca698d4f3bedb73f93e2cbdee91
memorylorry <a214966>
2021-06-18 16:13:59 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name='outer',
+    version='1.0',
+    description='A  module to manage output',
+    author='memorylorry',
+    author_email='memorylorry@outlook.com',
+    packages=setuptools.find_packages(),
+)
