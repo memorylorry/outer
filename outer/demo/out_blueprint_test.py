bf0c413185b4a83b395b942955b425edd3171e24
memorylorry <a214966>
2021-07-08 18:24:49 +0800

    dt parameter are replaced by key

--- outer/demo/out_blueprint_test.py
+++ outer/demo/out_blueprint_test.py
@@ -6,6 +6,6 @@ from BluePrintSample import BluePrintSample
 # composite blueprint
 blueprint = BluePrintSample('1')
 # touch file or dir
-out = blueprint.TRAIN_IMG_OUTPUT.touch_(2)
+out = blueprint.TRAIN_IMG_OUTPUT.touch()
 # std out
 print(out)
